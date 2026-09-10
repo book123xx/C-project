9560c9e603930d047b32051d47bab3510b17603d
Sara <sara123xx@gmail.com>
2026-09-10 12:01:24 +0300

    adedd from br_1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿
 Console.WriteLine("Hello, World!");
 Console.WriteLine("the read book !");
+Console.WriteLine("Hello, World!"+ 00000000000000);
+Console.WriteLine("the read book !");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("the read book !");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("the read book !");
