3cbc370e1b125db9464a2367e58bf6288fad9d9a
Sara <sara123xx@gmail.com>
2026-09-11 00:10:06 +0300

    Update console output messages in main program
    Removed old book message and added multiple Hello, World! outputs with variations, including string concatenation with an integer.

--- Program.cs
+++ Program.cs
@@ -2,5 +2,7 @@
 Console.WriteLine("the read book !");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!xxxxxxxxxxx");
-Console.WriteLine("Hello, World! cccccccccccc");
 Console.WriteLine("Hello, World!"+10);
+
+
+
