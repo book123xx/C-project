237ec5d2daf9cb4755fb66db19e7c61710a21087
Sara <sara123xx@gmail.com>
2026-09-10 12:25:23 +0300

    xxx

--- Program.cs
+++ Program.cs
@@ -1,20 +1,9 @@
 ﻿
-<<<<<<< HEAD
-=======
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
->>>>>>> br_w
-
-
-
-
-
-<<<<<<< HEAD
-
-
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!"+10);
-=======
->>>>>>> br_w
+
+
