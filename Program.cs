a4eedc3a99933d2a7505e27e4704cda3f93dc0d5
Sara <sara123xx@gmail.com>
2026-09-10 12:10:56 +0300

    a

--- Program.cs
+++ Program.cs
@@ -1,10 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("the read book !");
-Console.WriteLine("Hello, World!" +111111111);
-Console.WriteLine("the read book !"+3);
-Console.WriteLine("Hello, World!");
-Console.WriteLine("the read book !");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("the read book !");
