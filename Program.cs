8a826803cdb1381206657633c4cea558a451e111
Sara <sara123xx@gmail.com>
2026-09-10 12:20:10 +0300

    new master

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
 
 
 
 
 
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
