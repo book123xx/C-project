3d0d7e4e02a4844e70b5055cb328ebdf683386ba
Sara <sara123xx@gmail.com>
2026-09-11 22:31:15 +0300

    commit is new

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿
 Console.WriteLine("Hello, World!");
 Console.WriteLine("the read book !");
-Console.WriteLine("Hello, World!"+ 000000000000);
+Console.WriteLine("Hello, World!");
 Console.WriteLine("the read book !");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!xxxxxxxx");
 Console.WriteLine(Console.ReadLine());
+
 Console.WriteLine("Hello, World!"+10);
 
 
